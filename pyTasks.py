8758c979e492b2791bed13dbf46fd8bb4d2869f4
sagadevanmi <49581463+sagadevanmi@users.noreply.github.com>
2021-08-16 19:22:54 +0530

    Add files via upload
    Initial commit

---
+++ pyTasks.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+from google.cloud import storage
+from io import StringIO
+import pandas as pd
+
+client = storage.Client()
+bucket = client.get_bucket("de-gcp-training-2021/Final_Assignment_Dataset/")
+blob = bucket.get_blob("DE-2021-Batch1-GCP-Final-Assignment-Dataset.csv")
+
+bt = blob.download_as_string()
+
+s = str(bt, "utf-8")
+s = StringIO(s)
+df = pd.read_csv(s)
+
+
+df['timestamp_local_converted'] = pd.to_datetime(df['unix_timestamp_local'],unit='s')
+
+df['timestamp_utc_converted'] = pd.to_datetime(df['unix_timestamp_utc'],unit='s')
+
+df.to_csv('DE-2021-Batch1-GCP-Final-Assignment-Dataset.csv')
+
+df.drop_duplicates(subset=None, inplace=True)
+df.to_csv(file_name_output, index=False)
+
+
+df = pd.read_csv('DE-2021-Batch1-GCP-Final-Assignment-Dataset.csv')
+
+for i in set(df['date_local']):
+    df.loc[df['date_local'] == i].to_csv(f"{i}.csv",index=False)
+
+bucket = client.get_bucket('<your-bucket-name>')
+blob = bucket.blob('02-03-2019.csv')
+blob1 = bucket.blob('03-03-2019.csv')
+blob2 = bucket.blob('04-03-2019.csv')
+blob3 = bucket.blob('05-03-2019.csv')
+blob4 = bucket.blob('06-03-2019.csv')
+
+blob.upload_from_filename('02-03-2019.csv')
+blob1.upload_from_filename('03-03-2019.csv')
+blob2.upload_from_filename('04-03-2019.csv')
+blob3.upload_from_filename('05-03-2019.csv')
+blob4.upload_from_filename('06-03-2019.csv')
